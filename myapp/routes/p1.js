ecfd74fa3f46a738ac257a552027a7aa9ec4d41a
myungsubsim <myungsubsim@idatabank.com>
2017-01-26 13:50:16 +0900

    myapp

--- myapp/routes/p1.js
+++ myapp/routes/p1.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var cheerio = require('cheerio');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-  var url = "http://localhost:8080/board/home.page";
+  var url = "http://www.melon.com/chart/index.htm";
 
-  request(url,function(error,res,body){
+  request(url,function(error,res,html){
     if(error) throw error;
-      console.log(body);
+
+      var $ = cheerio.load(html);
+      var cnt = 0;
+      console.log(html);
+      $("#lst50").each(function(){
+        cnt++;
+      });
+
+      console.log(cnt);
   });
 });
 
